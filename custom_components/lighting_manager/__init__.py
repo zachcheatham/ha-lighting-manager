548197b4d57cfadd530827d909420d2b4ed47cca
Zach Cheatham <zachcheatham@gmail.com>
2022-08-08 16:15:45 -0500

    Changes to support WLED lights. This is really a state manager now.

--- custom_components/lighting_manager/__init__.py
+++ custom_components/lighting_manager/__init__.py
@@ -7,30 +7,11 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_ON,
 )
-from homeassistant.components.light import (
-    ATTR_COLOR_MODE,
-    ATTR_BRIGHTNESS,
-    ATTR_BRIGHTNESS_PCT,
-    ATTR_HS_COLOR,
-    ATTR_RGB_COLOR,
-    ATTR_RGBW_COLOR,
-    ATTR_RGBWW_COLOR,
-    ATTR_COLOR_TEMP,
-    ATTR_EFFECT,
-    ATTR_KELVIN,
-    ATTR_WHITE,
-    ATTR_WHITE_VALUE,
-    ATTR_XY_COLOR,
-    ATTR_FLASH,
-    LIGHT_TURN_ON_SCHEMA,
-)
 
 from homeassistant import core as ha
 from homeassistant.core import Config, Context, HomeAssistant, ServiceCall, State, callback
-from homeassistant.components.light import DOMAIN as DOMAIN_LIGHT
 from homeassistant.components.group import DOMAIN as DOMAIN_GROUP
 from homeassistant.components.sensor import DOMAIN as DOMAIN_SENSOR
-from homeassistant.components.scene import DOMAIN as DOMAIN_SCENE
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.state import async_reproduce_state
@@ -44,44 +25,54 @@ DATA_ENTITIES = "lm_entities"
 DATA_STATES = "lm_states"
 DATA_EVENT_LISTENER = "event_listener"
 DATA_HA_SCENE = "homeassistant_scene"
+DATA_CONF = "conf"
 
 SERVICE_INSERT_SCENE = "insert_scene"
 SERVICE_INSERT_STATE = "insert_state"
 SERVICE_REMOVE_LAYER = "remove_layer"
+SERVICE_REFRESH_ALL = "refresh_all"
 
 ATTR_PRIORITY = "priority"
 ATTR_ATTRIBUTES = "attributes"
 ATTR_CLEAR_LAYER = "clear_layer"
 
+CONF_ACTIVE_LAYER_ENTITY = "active_layer_entity"
+
 SIGNAL_LAYER_UPDATE=f"{DOMAIN}-update"
+
+ENTITY_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_ACTIVE_LAYER_ENTITY, default=False): cv.boolean,
+    }
+)
 
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
-                vol.Required(CONF_ENTITIES): vol.All(cv.ensure_list, [cv.string]),
+                vol.Required(CONF_ENTITIES): {cv.entity_id: vol.Any(None, ENTITY_SCHEMA)},
             }
         )
     },
     extra=vol.ALLOW_EXTRA,
 )
 
-SUPPORTED_LIGHT_ATTRS = [
-    ATTR_COLOR_MODE,
-    ATTR_BRIGHTNESS,
-    ATTR_BRIGHTNESS_PCT,
-    ATTR_HS_COLOR,
-    ATTR_RGB_COLOR,
-    ATTR_RGBW_COLOR,
-    ATTR_RGBWW_COLOR,
-    ATTR_COLOR_TEMP,
-    ATTR_EFFECT,
-    ATTR_KELVIN,
-    ATTR_WHITE,
-    ATTR_WHITE_VALUE,
-    ATTR_XY_COLOR,
-    ATTR_FLASH,
-]
+# SUPPORTED_LIGHT_ATTRS = [
+#     ATTR_COLOR_MODE,
+#     ATTR_BRIGHTNESS,
+#     ATTR_BRIGHTNESS_PCT,
+#     ATTR_HS_COLOR,
+#     ATTR_RGB_COLOR,
+#     ATTR_RGBW_COLOR,
+#     ATTR_RGBWW_COLOR,
+#     ATTR_COLOR_TEMP,
+#     ATTR_EFFECT,
+#     ATTR_KELVIN,
+#     ATTR_WHITE,
+#     ATTR_WHITE_VALUE,
+#     ATTR_XY_COLOR,
+#     ATTR_FLASH,
+# ]
 
 SERVICE_INSERT_SCENE_SCHEMA = vol.Schema(
     {
@@ -97,7 +88,8 @@ SERVICE_INSERT_STATE_SCHEMA = vol.Schema(
         vol.Required(ATTR_ENTITY_ID): cv.string,
         vol.Required(ATTR_PRIORITY): cv.positive_int,
         vol.Required(ATTR_ID): cv.string,
-        vol.Required(ATTR_ATTRIBUTES): LIGHT_TURN_ON_SCHEMA,
+        vol.Optional(ATTR_STATE): cv.string,
+        vol.Optional(ATTR_ATTRIBUTES): dict,
         vol.Optional(ATTR_CLEAR_LAYER): cv.boolean
     }
 )
@@ -115,13 +107,17 @@ def setup(hass: HomeAssistant, config: Config):
     hass.data[DOMAIN][DATA_ENTITIES] = conf[CONF_ENTITIES]
     hass.data[DOMAIN][DATA_STATES] = {}
 
-    for entity_id in conf[CONF_ENTITIES]:
+    for entity_id in conf[CONF_ENTITIES].keys():
         hass.data[DOMAIN][DATA_STATES][entity_id] = {}
 
-    def render_light(entity_id: str):
+    def render_entity(entity_id: str):
 
         if len(hass.data[DOMAIN][DATA_STATES][entity_id]) == 0:
+            #domain = ha.split_entity_id(entity_id)[0]
+            #if domain == "light" or domain == "group":
             return State(entity_id, STATE_OFF)
+            #else:
+            #    return 
         else:
             active_state = None
             for layer in hass.data[DOMAIN][DATA_STATES][entity_id]:
@@ -134,10 +130,10 @@ def setup(hass: HomeAssistant, config: Config):
 
             return active_state[ATTR_STATE]
 
-    async def apply_lights(entities: List, additional_states: List, context: Context):
+    async def apply_entities(entities: List, additional_states: List, context: Context):
         states = additional_states
         for entity_id in entities:
-            states.append(render_light(entity_id))
+            states.append(render_entity(entity_id))
 
         await async_reproduce_state(hass, states, context=context)
 
@@ -197,7 +193,7 @@ def setup(hass: HomeAssistant, config: Config):
             else:
                 non_managed_entities.append(ungrouped_entity_states[entity_id])
 
-        await apply_lights(affected_entities, non_managed_entities, call.context)
+        await apply_entities(affected_entities, non_managed_entities, call.context)
 
     hass.services.register(
         DOMAIN, SERVICE_INSERT_SCENE, insert_scene, SERVICE_INSERT_SCENE_SCHEMA
@@ -208,7 +204,8 @@ def setup(hass: HomeAssistant, config: Config):
         entity_id = call.data.get(ATTR_ENTITY_ID)
         priority = call.data.get(ATTR_PRIORITY)
         layer_id = call.data.get(ATTR_ID)
-        attributes = call.data.get(ATTR_ATTRIBUTES)
+        state = call.data.get(ATTR_STATE, STATE_ON)
+        attributes = call.data.get(ATTR_ATTRIBUTES, {})
         should_clear = call.data.get(ATTR_CLEAR_LAYER)
 
         affected_clear_entities = None
@@ -219,24 +216,22 @@ def setup(hass: HomeAssistant, config: Config):
             affected_clear_entities = clear_layer(layer_id)
 
         if ha.split_entity_id(entity_id)[0] == DOMAIN_GROUP:
-            for light_entity in hass.components.group.get_entity_ids(
-                entity_id, DOMAIN_LIGHT
-            ):
-                if light_entity in hass.data[DOMAIN][DATA_ENTITIES]:
-                    affected_entities.append(light_entity)
+            for entity in hass.components.group.get_entity_ids(entity_id):
+                if entity in hass.data[DOMAIN][DATA_ENTITIES]:
+                    affected_entities.append(entity)
                 else:
-                    extra_entities.append(light_entity)
+                    extra_entities.append(entity)
         elif entity_id in hass.data[DOMAIN][DATA_ENTITIES]:
             affected_entities.append(entity_id)
 
-        for light_entity_id in affected_entities:
-            hass.data[DOMAIN][DATA_STATES][light_entity_id][layer_id] = {
+        for entity in affected_entities:
+            hass.data[DOMAIN][DATA_STATES][entity][layer_id] = {
                 ATTR_PRIORITY: priority,
-                ATTR_STATE: State(entity_id, STATE_ON, attributes),
+                ATTR_STATE: State(entity_id, state, attributes),
             }
 
         extra_states = [
-            State(extra_entity_id, STATE_ON, attributes)
+            State(extra_entity_id, state, attributes)
             for extra_entity_id in extra_entities
         ]
 
@@ -244,7 +239,7 @@ def setup(hass: HomeAssistant, config: Config):
             affected_entities = list(set(affected_entities + affected_clear_entities))
 
         if len(affected_entities) > 0 or len(extra_states) > 0:
-            await apply_lights(affected_entities, extra_states, call.context)
+            await apply_entities(affected_entities, extra_states, call.context)
 
     hass.services.register(
         DOMAIN, SERVICE_INSERT_STATE, insert_state, SERVICE_INSERT_STATE_SCHEMA
@@ -259,9 +254,7 @@ def setup(hass: HomeAssistant, config: Config):
 
         if entity_id:
             if ha.split_entity_id(entity_id)[0] == DOMAIN_GROUP:
-                for light_entity in hass.components.group.get_entity_ids(
-                    entity_id, DOMAIN_LIGHT
-                ):
+                for light_entity in hass.components.group.get_entity_ids(entity_id):
                     if (
                         light_entity in hass.data[DOMAIN][DATA_ENTITIES]
                         and layer_id in hass.data[DOMAIN][DATA_STATES][light_entity]
@@ -281,10 +274,19 @@ def setup(hass: HomeAssistant, config: Config):
             hass.data[DOMAIN][DATA_STATES][light_entity_id].pop(layer_id)
 
         if len(affected_entities) > 0:
-            await apply_lights(affected_entities, [], call.context)
+            await apply_entities(affected_entities, [], call.context)
 
     hass.services.register(
         DOMAIN, SERVICE_REMOVE_LAYER, remove_layer, SERVICE_REMOVE_LAYER_SCHEMA
+    )
+
+    @callback
+    async def refresh_all(call: ServiceCall):
+        for entity_id in hass.data[DOMAIN][DATA_STATES].keys():
+            render_entity(entity_id)
+
+    hass.services.register(
+        DOMAIN, SERVICE_REFRESH_ALL, refresh_all
     )
 
     hass.helpers.discovery.load_platform(DOMAIN_SENSOR, DOMAIN, {}, config)
--- custom_components/lighting_manager/sensor.py
+++ custom_components/lighting_manager/sensor.py
@@ -1,20 +1,24 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from . import ATTR_PRIORITY, DATA_STATES, SIGNAL_LAYER_UPDATE
+from . import ATTR_PRIORITY, CONF_ACTIVE_LAYER_ENTITY, DATA_ENTITIES, DATA_STATES, SIGNAL_LAYER_UPDATE
+import logging
 
 DOMAIN="lighting_manager"
+
+_LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     add_entities(
         [
             ActiveLayerSensor(entity_id, hass.data[DOMAIN][DATA_STATES][entity_id])
-            for entity_id in hass.data[DOMAIN][DATA_STATES]
+            for entity_id in hass.data[DOMAIN][DATA_ENTITIES].keys()
+            if (hass.data[DOMAIN][DATA_ENTITIES][entity_id] is not None and hass.data[DOMAIN][DATA_ENTITIES][entity_id][CONF_ACTIVE_LAYER_ENTITY])
         ]
     )
 
 class ActiveLayerSensor(SensorEntity):
     def __init__(self, light_entity_id: str, layer_data: dict):
-        self._attr_name = light_entity_id + " Lighting Manager Active Layer"
+        self._attr_name = light_entity_id + " Active Layer"
         self._attr_should_poll = False
         self._layers = layer_data
         self._light_entity_id = light_entity_id
